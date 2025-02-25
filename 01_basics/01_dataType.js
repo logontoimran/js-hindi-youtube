e2077d50dd1914e0d4625eb774d319b02d471de6
logontoimran <38036457+logontoimran@users.noreply.github.com>
2025-02-25 11:41:42 +0000

    datatype

---
+++ 01_basics/01_dataType.js
@@ -0,0 +1,23 @@
+"use strict";//treat all JS code as newer version
+// alert(3 + 3) //we are usin node js,not browser
+
+//pls code readibilty high
+console.log(3
+    +3)//not good practice
+    //tc39.es//mdm document
+    
+console.log("imran")
+let name = "imran"
+let age =28
+let isLoggedIn =true/false
+//number =>?2 to power 53
+//bigint
+//string =""
+//boolean =>true/false
+//null => standlone vaalue
+//undefined =>not initialize
+//symbole =>unique when create multiple componets like react componenets use
+
+//object
+console.log(typeof undefined)//undefined
+console.log(typeof null)//type-object
