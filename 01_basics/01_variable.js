1460b62aeb246ad94437a03a263e78c4390b0767
logontoimran <38036457+logontoimran@users.noreply.github.com>
2025-02-25 11:42:02 +0000

    datatype

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,14 @@
+const accountId = 1120
+let accountEmail = "imran@gmail.com"
+var accountPassword =101
+accountCity ="kanpur"
+
+let accountState;
+
+//accountId =200
+/*
+var nor use into js beacuse scoope issue in function scope
+*/
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
